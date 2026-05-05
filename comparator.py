b2ac74090d10c4435930a09aeef6de7f5c9324dc
Vitor <jose.vitorherrera@gmail.com>
2026-05-05 19:52:49 -0300

    Add files via upload

---
+++ comparator.py
@@ -0,0 +1,11 @@
+valor_a = float(input("Digite o Valor A: "))
+valor_b = float(input("Digite o Valor B: "))
+
+if valor_a > valor_b:
+    print("A É Maior")
+elif valor_a < valor_b:
+    print("B É Maior") 
+else:
+    print("São Iguais")
+
+    
